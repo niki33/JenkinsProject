415791a33739bd88d0c8485a479a88e44e92daa0
Nikita Chaudhary <chanik02@CSGICORP.COM>
2018-09-19 12:04:39 +0530

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateMessage());
+
+
+          
+        }
+        public static string CreateMessage()
+        {
+            return "Hello World";
+        }
+    }
+}
